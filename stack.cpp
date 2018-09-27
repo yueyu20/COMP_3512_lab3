b86c02db446d5aea6a3d3ffdaf6b6d7efaf7e622
Yue Yu <yueyu1015@gmail.com>
2018-09-27 13:10:12 -0700

    some functions added

--- stack.cpp
+++ stack.cpp
@@ -1,4 +1,40 @@
 //
 // Created by 虞跃 on 2018-09-27.
 //
+#include "stack.hpp"
+#include <iostream>
 
+using namespace std;
+
+stack::stack() {
+    top = -1;
+
+}
+
+bool stack::push(int x) {
+    if (top > MAX) {
+        cout << "Stack Overflow";
+        return false;
+    } else{
+        arr[++top] = x;
+        cout << "pushed into stack\n";
+        return true;
+    }
+}
+
+void stack::pop() {
+    if(top < 0) {
+        cout << "Stack Underflow";
+    } else {
+        --top;
+    }
+}
+
+int stack::getTop() const {
+    int x = arr[top];
+    return x;
+}
+
+bool stack::isEmpty() const {
+
+}
--- stack.hpp
+++ stack.hpp
@@ -5,4 +5,20 @@
 #ifndef COMP_3512_LAB3_STACK_HPP
 #define COMP_3512_LAB3_STACK_HPP
 
+const int MAX = 10;
+
+class stack {
+private:
+    int top;
+    int arr [MAX];
+public:
+    stack();
+    bool push(int);
+    void pop();
+    int getTop() const;
+    bool isEmpty() const;
+
+
+};
+
 #endif //COMP_3512_LAB3_STACK_HPP
