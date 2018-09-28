b6852c8460df86bc3214a13e3273141af32d03a3
Yue Yu <yueyu1015@gmail.com>
2018-09-27 20:20:23 -0700

    catch.hpp

---
+++ units_tests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 虞跃 on 2018-09-27.
+//
+
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() put this in one cpp file
+
+
+#include "catch2.hpp"
+#include "stack.hpp"
